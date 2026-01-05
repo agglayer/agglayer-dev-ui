b4e3382741174b134d2acf56bd512d7952593804
Shan8851.eth <shan8851@proton.me>
2026-01-05 13:25:53 +0000

    add packages, basic utils and prettier setup

---
+++ app/utils/address.ts
@@ -0,0 +1,6 @@
+export const isValidEthereumAddress = (address: string): boolean => /^0x[a-fA-F0-9]{40}$/.test(address);
+
+export const isSameAddress = (address1: string | undefined, address2: string | undefined): boolean => {
+  if (!address1 || !address2) return false;
+  return address1.trim().toLowerCase() === address2.trim().toLowerCase();
+};
